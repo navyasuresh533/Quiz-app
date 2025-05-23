0c9ea04a962598d3ba56e5223a07bcc2a5eca50f
navyasuresh533 <156298239+navyasuresh533@users.noreply.github.com>
2025-05-23 18:08:37 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
+}
